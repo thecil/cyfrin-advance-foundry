6fc4cdcc4fe0f3b227fd127c663b0f7aa1d2c3fc
thecil <thecilsurf@gmail.com>
2025-04-01 14:41:28 -0400

    mood nft - init

---
+++ script/section-two-erc721/DeployMoodNft.s.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {MoodNft} from "../../src/section-two-erc721/MoodNft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNft is Script {
+
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./images/sad.svg");
+        string memory happySvg = vm.readFile("./images/happy.svg");
+
+        vm.startBroadcast();
+        MoodNft nft = new MoodNft(
+            "MoodNft",
+            "MOOD",
+            svgToImageUri(sadSvg),
+            svgToImageUri(happySvg)
+        );
+        vm.stopBroadcast();
+        return nft;
+    }
+
+    function svgToImageUri(
+        string memory svg
+    ) public pure returns (string memory) {
+        string memory baseURL = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(
+            bytes(string(abi.encodePacked(svg)))
+        );
+
+        return string(abi.encodePacked(baseURL, svgBase64Encoded));
+    }
+}
---
+++ src/section-two-erc721/MoodNft.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract MoodNft is ERC721, Ownable {
+    error MoodNft__CantFlipMoodIfNotOwner();
+
+    uint256 private s_tokenCounter;
+    string private s_sadSvgImageUri;
+    string private s_happySvgImageUri;
+
+    enum Mood {
+        SAD,
+        HAPPY
+    }
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory sadSvgImageUri,
+        string memory happySvgImageUri
+    ) ERC721(_name, _symbol) Ownable(msg.sender) {
+        s_tokenCounter = 1;
+        s_sadSvgImageUri = sadSvgImageUri;
+        s_happySvgImageUri = happySvgImageUri;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY; // default to HAPPY mood
+        s_tokenCounter++;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        if (getApproved(tokenId) != msg.sender && ownerOf(tokenId) != msg.sender) {
+            revert MoodNft__CantFlipMoodIfNotOwner();
+        }
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
+    }
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        if (ownerOf(tokenId) == address(0)) {
+            revert ERC721NonexistentToken(tokenId);
+        }
+        string memory imageURI = s_happySvgImageUri;
+
+        if (s_tokenIdToMood[tokenId] == Mood.SAD) {
+            imageURI = s_sadSvgImageUri;
+        }
+        return
+            string(
+                abi.encodePacked(
+                    _baseURI(),
+                    Base64.encode(
+                        bytes( // bytes casting actually unnecessary as 'abi.encodePacked()' returns a bytes
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(), // You can add whatever name here
+                                '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                                '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
+                )
+            );
+    }
+}
