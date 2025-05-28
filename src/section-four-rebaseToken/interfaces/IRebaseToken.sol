eb90bab90bb576280682ff2791b6a71e02187204
thecil <thecilsurf@gmail.com>
2025-05-28 02:33:34 -0400

    Update IRebaseToken.sol

--- src/section-four-rebaseToken/interfaces/IRebaseToken.sol
+++ src/section-four-rebaseToken/interfaces/IRebaseToken.sol
@@ -1,35 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-interface IRebaseToken{
-    
+interface IRebaseToken {
     /**
      * @notice Get the principle balance of a user.
      * This is the number of tokens that have currently minted to the user, not including any interest that has accrued since the last time the user interacted with the protocol.
      * @param _user the
      */
     function principleBalanceOf(address _user) external view returns (uint256);
-    
+
     /**
      * @notice Mint the user tokens when they deposit into the vault
      * @param _to The user to mint the tokens to
      * @param _amount The amount of tokens to mint
      */
-    function mint(
-        address _to,
-        uint256 _amount
-    ) external;
-    
+    function mint(address _to, uint256 _amount) external;
+
     /**
      * @notice Burn the user tokens when they withdraw from the vault
      * @param _from The user to burn the tokens from
      * @param _amount The amount of tokens to burn
      */
-    function burn(
-        address _from,
-        uint256 _amount
-    ) external;
-    
+    function burn(address _from, uint256 _amount) external;
+
     /**
      * @notice Transfer tokens from one user to another
      * @param _recipient The user to transfer the tokens to
@@ -60,13 +53,11 @@ interface IRebaseToken{
      * @return The interest rate for the contract
      */
     function getInterestRate() external view returns (uint256);
-    
+
     /**
      * @notice Get the interest rate for the user
      * @param _user The user to get the interest rate for
      * @return The interest rate for the user
      */
-    function getUserInterestRate(
-        address _user
-    ) external view returns (uint256) 
+    function getUserInterestRate(address _user) external view returns (uint256);
 }
