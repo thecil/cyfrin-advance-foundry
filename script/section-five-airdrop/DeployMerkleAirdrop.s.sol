d6adb87152b3342124ed06f5981c5fb7bbf618c5
thecil <thecilsurf@gmail.com>
2025-06-27 14:59:14 -0400

    Create DeployMerkleAirdrop.s.sol

---
+++ script/section-five-airdrop/DeployMerkleAirdrop.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Script} from "forge-std/Script.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MerkleAirdrop} from "../../src/section-five-airdrop/MerkleAirdrop.sol";
+import {BagelToken} from "../../src/section-five-airdrop/BagelToken.sol";
+
+contract DeployMerkleAirdropScript is Script {
+    bytes32 private s_merkleRoot =
+        0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    uint256 private s_amountToTransfer = 4 * 25 * 1e18;
+
+    function deployMerkleAirdrop()
+        public
+        returns (MerkleAirdrop airdrop, BagelToken token)
+    {
+        vm.startBroadcast();
+        token = new BagelToken();
+        airdrop = new MerkleAirdrop(s_merkleRoot, IERC20(address(token)));
+        token.mint(token.owner(), s_amountToTransfer);
+        token.transfer(address(airdrop), s_amountToTransfer);
+        vm.stopBroadcast();
+    }
+
+    function run() external returns (MerkleAirdrop, BagelToken) {
+        return deployMerkleAirdrop();
+    }
+}
