8cfe1a33bc19dddaf0888e60a18a0d21affb0395
thecil <thecilsurf@gmail.com>
2025-07-13 19:18:15 -0400

    Rename contract BoxV1 to BoxV2
    Updated the contract name from BoxV1 to BoxV2 in BoxV2.sol to reflect the new version for upgradeability purposes.

--- src/section-six-upgradable/BoxV2.sol
+++ src/section-six-upgradable/BoxV2.sol
@@ -5,7 +5,7 @@ import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/U
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+contract BoxV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
 
     /// @custom:oz-upgrades-unsafe-allow constructor
