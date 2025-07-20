f489bb55782d1ad612602bffb731811663f7833d
thecil <thecilsurf@gmail.com>
2025-07-20 17:01:45 -0400

    Add ZkMinimalAccount contract and zksync foundry scripts
    Introduces ZkMinimalAccount.sol implementing the IAccount interface for zkSync, with stubbed external functions. Adds foundryup-zksync installer script, install helper, and updates remappings for zkSync and era-contracts. Updates foundry.toml to include era-contracts remapping.

---
+++ src/sec-seven-acc-abstraction/zksync/ZkMinimalAccount.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import {IAccount} from "@era-contracts/interfaces/IAccount.sol";
+import {Transaction} from "@era-contracts/libraries/TransactionHelper.sol";
+
+contract ZkMinimalAccount is IAccount {
+    /*//////////////////////////////////////////////////////////////
+                           EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    /**
+     * @notice MUST increase the nonce
+     * @notice MUST validate transaction
+     */
+    function validateTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
+        external
+        payable
+        returns (bytes4 magic)
+    {}
+
+    function executeTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
+        external
+        payable
+    {}
+
+    function executeTransactionFromOutside(Transaction memory _transaction) external payable {}
+
+    function payForTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
+        external
+        payable
+    {}
+
+    function prepareForPaymaster(bytes32 _txHash, bytes32 _possibleSignedHash, Transaction memory _transaction)
+        external
+        payable
+    {}
+}
